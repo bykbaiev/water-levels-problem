afd12cf006c064b50189111fe5ed692164b34c9c
bykbaiev <oleksandr.bykbaiev@youscan.io>
2021-02-19 05:12:49 +0200

    UI refactoring

--- src/UI.ts
+++ src/UI.ts
@@ -16,25 +16,49 @@ const BOARD_SIZE = {
 
 const ENTER_KEY = 'Enter';
 
-const getSegmentEl = (width: number, height: number) => (segment: Segment): string => {
+const node = (elType: keyof HTMLElementTagNameMap) => (
+    props: Record<string, string>,
+    children: Array<Node>
+) => {
+    const el = document.createElement(elType);
+
+    Object.keys(props).map((name) => el.setAttribute(name, props[name]));
+    children.map(child => el.appendChild(child));
+
+    return el;
+};
+
+const div = node('div');
+
+const text = (value: string) => document.createTextNode(value);
+
+const getSegmentEl = (width: number, height: number) => (
+    segment: Segment
+): HTMLElement => {
     const { height: segmentHeight, water } = segment;
 
-    return `
-        <div class="segment" style="width: ${width}px">
-            <div class="total">${segmentHeight + water}</div>
-            <div class="water" style="height: ${water * height}px"></div>
-            <div class="ground" style="height: ${segmentHeight * height}px"></div>
-            <div class="water-level">${water}</div>
-        </div>
-    `
-}
+    return div({ class: 'segment', style: `width:${width}px` }, [
+        div({ class: 'total' }, [text((segmentHeight + water).toFixed(3))]),
+        div({ class: 'water', style: `height: ${water * height}px` }, []),
+        div({ class: 'ground', style: `height: ${segmentHeight * height}px` }, []),
+        div({ class: 'water-level' }, [text(water.toFixed(3))])
+    ]);
+};
 
-const getLandscapeEl = (landscape: Array<Segment>): string => {
+const getLandscapeEl = (landscape: Array<Segment>): HTMLElement => {
     const { length } = landscape;
-    const maxHeight = Math.max(...landscape.map(({ height, water }) => height + water));
+    const maxHeight = Math.max(
+        ...landscape.map(({ height, water }) => height + water)
+    );
 
     if (length === 0) {
-        return `<div class="empty" style="width: ${BOARD_SIZE.WIDTH}px; height: ${BOARD_SIZE.HEIGHT}px; line-height: ${BOARD_SIZE.HEIGHT}px;">Please, provide the landscape</div>`;
+        return div(
+            {
+                class: 'empty',
+                style: `width: ${BOARD_SIZE.WIDTH}px; height: ${BOARD_SIZE.HEIGHT}px; line-height: ${BOARD_SIZE.HEIGHT}px;`,
+            },
+            [text('Please provide the landscape')]
+        );
     }
 
     const heightLimit = Math.floor(1.3 * maxHeight);
@@ -43,30 +67,66 @@ const getLandscapeEl = (landscape: Array<Segment>): string => {
 
     const getSegmentElWithSize = getSegmentEl(segmentWidth, segmentHeight);
 
-    return `
-        <div class="board-wrapper" style="width: ${BOARD_SIZE.WIDTH}px; height: ${BOARD_SIZE.HEIGHT}px;">
-            ${Array(heightLimit).fill(0).map((_, i) => `<div class="line" style="height:${segmentHeight}px"></div>`).join('')}
-            <div class="board">
-                ${landscape.map(getSegmentElWithSize).join('')}
-                <div class="ruler">
-                    ${Array(heightLimit).fill(0).map((_, i) => `<div class="ruler-item" style="height:${segmentHeight}px"><div class="ruler-number" style="bottom:-10px">${i}</div></div>`).join('')}
-                </div>
-            </div>
-        </div>
-    `;
-}
-
-export const renderLandscape = (landscape: Array<Segment>): void => {
-    document.querySelector(QUERY.LANDSCAPE).innerHTML = getLandscapeEl(landscape);
+    return div(
+        {
+            class: 'board-wrapper',
+            style: `width: ${BOARD_SIZE.WIDTH}px; height: ${BOARD_SIZE.HEIGHT}px;`,
+        },
+        [
+            ...Array(heightLimit)
+                .fill(0)
+                .map(() =>
+                    div(
+                        { class: 'line', style: `height:${segmentHeight}px` },
+                        []
+                    )
+                ),
+            div({ class: 'board' }, [
+                ...landscape.map(getSegmentElWithSize),
+                div(
+                    { class: 'ruler' },
+                    Array(heightLimit)
+                        .fill(0)
+                        .map((_, i) =>
+                            div(
+                                {
+                                    class: 'ruler-item',
+                                    style: `height:${segmentHeight}px`,
+                                },
+                                [
+                                    div(
+                                        {
+                                            class: 'ruler-number',
+                                            style: 'bottom:-10px',
+                                        },
+                                        [text(i.toString())]
+                                    ),
+                                ]
+                            )
+                        )
+                ),
+            ]),
+        ]
+    );
 };
 
-export const addListeners = (onCalc: (heights: string, time: string) => void) => {
+export const renderLandscape = (landscape: Array<Segment>): void => {
+    const el = document.querySelector(QUERY.LANDSCAPE);
+
+    el!.innerHTML = '';
+    el!.append(getLandscapeEl(landscape));
+};
+
+export const addListeners = (
+    onCalc: (heights: string, time: string) => void
+) => {
     const handler = () => {
         hideErrorMsg();
         hideResultMsg();
         onCalc(
-            document.querySelector<HTMLInputElement>(QUERY.LANDSCAPE_INPUT).value,
-            document.querySelector<HTMLInputElement>(QUERY.TIME_INPUT).value
+            document.querySelector<HTMLInputElement>(QUERY.LANDSCAPE_INPUT)!
+                .value,
+            document.querySelector<HTMLInputElement>(QUERY.TIME_INPUT)!.value
         );
     };
 
@@ -76,23 +136,27 @@ export const addListeners = (onCalc: (heights: string, time: string) => void) =>
         }
     };
 
-    document.querySelector(QUERY.START_BTN).addEventListener('click', handler);
-    document.querySelector(QUERY.LANDSCAPE_INPUT).addEventListener('keypress', keyPressHandler);
-    document.querySelector(QUERY.TIME_INPUT).addEventListener('keypress', keyPressHandler);
-}
+    document.querySelector(QUERY.START_BTN)!.addEventListener('click', handler);
+    document
+        .querySelector(QUERY.LANDSCAPE_INPUT)!
+        .addEventListener('keypress', keyPressHandler);
+    document
+        .querySelector(QUERY.TIME_INPUT)!
+        .addEventListener('keypress', keyPressHandler);
+};
 
 export const showErrorMsg = (msg: string): void => {
-    document.querySelector(QUERY.ERROR_MSG).innerHTML = msg;
+    document.querySelector(QUERY.ERROR_MSG)!.innerHTML = msg;
 };
 
 const hideErrorMsg = (): void => {
-    document.querySelector(QUERY.ERROR_MSG).innerHTML = '';
+    document.querySelector(QUERY.ERROR_MSG)!.innerHTML = '';
 };
 
 export const showResultMsg = (msg: string): void => {
-    document.querySelector(QUERY.RESULT_MSG).innerHTML = msg;
+    document.querySelector(QUERY.RESULT_MSG)!.innerHTML = msg;
 };
 
 const hideResultMsg = (): void => {
-    document.querySelector(QUERY.RESULT_MSG).innerHTML = '';
+    document.querySelector(QUERY.RESULT_MSG)!.innerHTML = '';
 };
